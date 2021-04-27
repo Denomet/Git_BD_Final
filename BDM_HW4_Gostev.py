128e5ba05c3ab80dfdb951ba5b8328625b8cb31a
Denomet <83107238+Denomet@users.noreply.github.com>
2021-04-27 03:43:24 -0400

    Update BDM_HW4_Gostev.py

--- BDM_HW4_Gostev.py
+++ BDM_HW4_Gostev.py
@@ -10,9 +10,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 
 if __name__=='__main__':
-    sc = SparkContext()
-
-    NAICS_dict = {
+    	sc = SparkContext()
+	
+	NAICS_dict = {
 		"452210": "big_box_grocers",
 		"452311": "big_box_grocers",
 		"445120": "convenience_stores",
@@ -52,46 +52,47 @@ if __name__=='__main__':
 
 	def MedLowHigh(x):
 		if len(x) > 0:
-	    	median_vst = statistics.median([item[1] for item in x[1]])
+	    		median_vst = statistics.median([item[1] for item in x[1]])
 
-	    if len(x[1]) == 1:
-	     	sd_vst = 0
-	    else:
-	     	sd_vst = statistics.stdev([item[1] for item in x[1]])
+	    	if len(x[1]) == 1:
+	    	 	sd_vst = 0
+	   	 else:
+	   	  	sd_vst = statistics.stdev([item[1] for item in x[1]])
 
-	    high_vst = median_vst + sd_vst
-	    low_vst = median_vst - sd_vst
-	    if low_vst < 0:
-	     	low_vst = 0
+		high_vst = median_vst + sd_vst
+	    	low_vst = median_vst - sd_vst
+	    	if low_vst < 0:
+	     		low_vst = 0
 
-	    real_year = datetime.datetime.strptime(x[0][0], '%Y-%m-%d').year
-	    project_date =  '2020-' + x[0][0][5:] 
-	    place_type = x[0][1] 
-	    return (real_year, project_date, int(median_vst), int(low_vst), int(high_vst), place_type)
+	    	real_year = datetime.datetime.strptime(x[0][0], '%Y-%m-%d').year
+		project_date =  '2020-' + x[0][0][5:] 
+		place_type = x[0][1] 
+		return (real_year, project_date, int(median_vst), int(low_vst), int(high_vst), place_type)
 
-		patterns = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020.csv') \
-			.map(lambda x: next(csv.reader([x]))) \
-			.filter(lambda x: x[1] in places_dict.keys()) \
-			.map(lambda x: (x[1], x[12], x[16] )) \
-			.flatMap(lambda x: convertKey(x)) \
-			.groupByKey() \
-			.map(lambda x: (x[0], list(x[1]) )) \
-			.map(lambda x: MedLowHigh(x)) \
-			.filter(lambda x: x[0] > 2018) \
-			.sortBy(lambda x: (x[0], x[1])) \
-			.collect() 
-		schema = StructType([ \
-		    StructField("year",StringType(),True), \
-		    StructField("date",StringType(),True), \
-		    StructField("median",IntegerType(),True), \
-		    StructField("low", IntegerType(), True), \
-		    StructField("high", IntegerType(), True), \
-		    StructField("type", StringType(), True)])
+	patterns = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020.csv') \
+		.map(lambda x: next(csv.reader([x]))) \
+		.filter(lambda x: x[1] in places_dict.keys()) \
+		.map(lambda x: (x[1], x[12], x[16] )) \
+		.flatMap(lambda x: convertKey(x)) \
+		.groupByKey() \
+		.map(lambda x: (x[0], list(x[1]) )) \
+		.map(lambda x: MedLowHigh(x)) \
+		.filter(lambda x: x[0] > 2018) \
+		.sortBy(lambda x: (x[0], x[1])) \
+		.collect() 
+	
+	schema = StructType([ \
+		StructField("year",StringType(),True), \
+		StructField("date",StringType(),True), \
+		StructField("median",IntegerType(),True), \
+		StructField("low", IntegerType(), True), \
+		StructField("high", IntegerType(), True), \
+		StructField("type", StringType(), True)])
 
-		spark = SparkSession.builder.getOrCreate()
-		df = spark.createDataFrame(data=patterns,schema=schema)
+	spark = SparkSession.builder.getOrCreate()
+	df = spark.createDataFrame(data=patterns,schema=schema)
 
-		df.write.option("header",True) \
-	        .partitionBy("type") \
-	        .mode("overwrite") \
-	        .csv("/test")
+	df.write.option("header",True) \
+		.partitionBy("type") \
+		.mode("overwrite") \
+		.csv("/test")
