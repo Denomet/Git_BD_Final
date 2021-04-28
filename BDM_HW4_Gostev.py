1af37327d45c0016c1420494d184dab82ffd35d2
Denomet <83107238+Denomet@users.noreply.github.com>
2021-04-28 01:26:25 -0400

    Update BDM_HW4_Gostev.py

--- BDM_HW4_Gostev.py
+++ BDM_HW4_Gostev.py
@@ -100,4 +100,4 @@ if __name__=='__main__':
     spark = SparkSession.builder.getOrCreate()
     df = spark.createDataFrame(data=patterns,schema=schema)
     print('sys.argv>>>>>>>>>',sys.argv)
-    df.write.option("header",False).partitionBy("type").mode("overwrite").csv(sys.argv[1] if len(sys.argv)>2 else  "hdfs:///user/dgostev/test")
+    df.write.option("header",False).partitionBy("type").mode("overwrite").csv(sys.argv[1] if len(sys.argv)>1 else  "hdfs:///user/dgostev/test")
